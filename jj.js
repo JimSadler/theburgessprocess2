baae5f6fbf910b56b46ee80d5e71f741539bf5f5
Jim Sadler <jimsadler@Jims-iMac.local>
2016-07-17 12:55:56 -0400

    Initial commit

---
+++ jj.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jsadler on 7/10/16.
+ */
+Dream host
+
+theburgessprocess@yahoo.com
+Toffel81715
+
+admin@mmarthaburgessnovak.net
+Jazz6251
---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*
+ * Created by jimsadler on 4/12/16.
+ */
+$(document).ready(function() {
+    var scroll_start = 0;
+    var startchange = $('.nav');
+    var offset = startchange.offset();
+    $(document).scroll(function() {
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > offset.top) {
+            $('.navbar').css('background-color', '#6A6A6A');
+        }else {
+            $('.navbar').css('background-color', 'transparent')
+        }
+    });
+});
+
+
+$( document ).ready(function() {
+    $('#logo').hide().one("load",function(){
+        $(this).fadeIn(2000);
+    }).each(function(){
+        if(this.complete) $(this).trigger("load");
+    });
+    $('.fade').addClass('fade-in');
+});
