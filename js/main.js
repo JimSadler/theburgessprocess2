44f2199157c9e3460198521465218d395816246e
Jim Sadler <jim@jimsadlermusic.com>
2016-10-08 13:24:27 -0400

    style changes, changed from container-fluid to container

--- js/main.js
+++ js/main.js
@@ -5,24 +5,31 @@ $(document).ready(function() {
     var scroll_start = 0;
     var startchange = $('.nav');
     var offset = startchange.offset();
-    $(document).scroll(function() {
+    $(document).scroll(function () {
         scroll_start = $(this).scrollTop();
-        if(scroll_start > offset.top) {
+        if (scroll_start > offset.top) {
             $('#navbar-index').css('background-color', 'rgba(2, 23, 40, 0.8)');
-        }else {
+        } else {
             $('#navbar-index').css('background-color', 'rgba(2, 23, 40, 0.8)')
 
         }
     });
 
-    $(document).on('click','.navbar-collapse.in',function(e) {
-        if( $(e.target).is('a') ) {
+    $(document).on('click', '.navbar-collapse.in', function (e) {
+        if ($(e.target).is('a')) {
             $(this).collapse('hide');
-            }
-        });
-  //added margin to h3
-    ele.find('h3:contains(2016)').css('margin-top','4em');
+        }
+    });
+}
+$(document).ready(function() {
+    $(init);
+
+    function init() {
+        $('a[href="http://www.blogtalkradio.com/marthaburgessnovak/2016/10/9/YouMatter."]').css('margin-bottom', '2em');
+    }
 });
+  //added margin to h3
+
 /*$( document ).ready(function() {
     $('#logo').hide().one("load",function(){
         $(this).fadeIn(2000);
